301d47c6e6227f8689d8ccc3e899a0f67a767b4d
Keith <keith.tzeng@quantatw.com>
2012-11-09 15:15:13 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Khtapp001::Application.config.secret_token = '24f0dcc89e41fd9d942c5ac8ba2894f639e6433fc4aa4a07f4831fb9b801ff58de6794fb7aaab3cb8130ace208e41bd643d6e39f383f2520d39acd07a5291d88'
